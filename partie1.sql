90300338b59ef9027eda1351343d5b07630a3bdc
Gugus18 <letang.e2404313@etud.univ-ubs.fr>
2025-04-02 11:20:09 +0200

    Update partie1.sql

--- partie1.sql
+++ partie1.sql
@@ -1,11 +1,14 @@
--- Liste des Quartiers qui possèdent un compteur
+-- 1) Liste des Quartiers qui possèdent un compteur
 SELECT DISTINCT UPPER(nomQuartier) nomQuartier
 FROM Quartiers
 INNER JOIN Compteurs ON idQuartier = unQuartier;
 
--- Les jours qui ont exactement le même nombre de vélos
+-- 2) Les jours qui ont exactement le même nombre de vélos
 SELECT C1.date Date1, C2.date Date2, C1.nombresVelos
 FROM Comptage_Velo C1
 JOIN Comptage_Velo C2 ON C1.nombresVelos = C2.nombresVelos;
 
---
+-- 3) 
+
+-- 4) 
+
